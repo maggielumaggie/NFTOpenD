74873ab548d87c65387216b7c977149dd27c8523
Mengqi Lu <mengqilu@mengqis-mbp.lan>
2022-02-04 15:59:19 -0800

    Add javadoc

---
+++ Desktop/CS5004/Lab1/src/test/java/AuthorTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthorTest {
+    private Author jane;
+    @BeforeEach
+    void setUp() {
+        this.jane = new Author("Jane Doe", "j@a.com",
+            "222 Main St, Seattle, Wa, 98980");
+    }
+
+    @Test
+    void getName() {
+        Assertions.assertEquals("Jane Doe", this.jane.getName());
+    }
+
+    @Test
+    void getEmail() {
+        //Assertions.fail("Not yet implemented");
+        Assertions.assertEquals("j@a.com", this.jane.getEmail());
+    }
+
+    @Test
+    void getAddress() {
+        //Assertions.fail("Not yet implemented");
+        Assertions.assertEquals("222 Main St, Seattle, Wa, 98980", this.jane.getAddress());
+    }
+}
